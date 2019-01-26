6240f65b7a0bcfd72a0a6a9ab8e076292f9fdb4b
Marleen Luijk, van <marleen.vanluijk@ogd.nl>
2019-01-26 11:35:51 +0100

    start project and add search-page

---
+++ Movies/Movies/Controllers/SearchController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Movies.Controllers
+{
+    public class SearchController : Controller
+    {
+        // GET: Search
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
